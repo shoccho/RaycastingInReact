bafbe2681df1e69e2d081f53cc7f353338b6625b
shoccho <sfb.shoccho@gmail.com>
2024-08-17 17:10:15 +0600

    fixed bug when there are gaps in the walls

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -7,11 +7,13 @@ export const Bar = ({ dist, backgroundColor }) => {
         const t = (x - x0) / (x1 - x0);
         return y0 + t * (y1 - y0);
     }
-
+    if(dist == Infinity){
+        return <div className="bar empty"></div>
+    }
     return <div
         className="bar"
         style={{
-        height: `${Math.min(400, 100 * 400/dist)}px`,
+        height: `${Math.min(100, 100 * 100/dist)}vh`,
         backgroundColor: `hsl(${backgroundColor},  100%, ${lerp(dist, 0, 600, 50, 5)}%)` 
         }} />
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,7 +10,8 @@ export const Game = () => {
     });
     //red  blue green orange
     const hslColors = [-1, 0, 240, 120, 30]
-    const colors = ["black", "red", "blue", "green"]
+    const colors = ["black", "red", "blue", "green", "aqua"]
+
 
     const map = [
         [1, 1, 1, 4, 4, 4, 1, 1],
@@ -22,6 +23,18 @@ export const Game = () => {
         [3, 0, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1],
     ];
+
+    // const floorMap = [
+    //     [1, 1, 1, 4, 4, 4, 1, 1],
+    //     [1, 3, 3, 3, 3, 3, 3, 1],
+    //     [1, 3, 3, 3, 3, 3, 3, 1],
+    //     [3, 3, 3, 3, 3, 2, 2, 2],
+    //     [3, 3, 3, 3, 3, 3, 3, 2],
+    //     [3, 3, 3, 3, 3, 3, 3, 2],
+    //     [3, 3, 3, 3, 3, 3, 3, 1],
+    //     [1, 1, 1, 1, 1, 1, 1, 1],
+    // ];
+
     const scale = 100;
 
     const WIDTH = map.length * 100;
@@ -29,7 +42,6 @@ export const Game = () => {
 
     const castRay = (angle) => {
         if (angle < 0) {
-            console.log(angle)
             angle += 2 * Math.PI;
         }
         if (angle > 2 * Math.PI) {
@@ -47,80 +59,87 @@ export const Game = () => {
             const mapY = Math.floor(py / scale);
 
             if (mapX < 0 || mapX >= map[0].length || mapY < 0 || mapY >= map.length) {
-                return -1;
+                return { distance: Infinity, color: 0 };
             }
 
             if (map[mapY][mapX] !== 0) {
                 let cameraAngle = player.a - angle;
-                if(cameraAngle < 0 ) cameraAngle+=2*Math.PI;
-                else if(cameraAngle > 2* Math.PI) cameraAngle -= 2* Math.PI;
-                return { distance: distance * Math.cos(cameraAngle) , color: hslColors[map[mapY][mapX]] };
+                if (cameraAngle < 0) cameraAngle += 2 * Math.PI;
+                else if (cameraAngle > 2 * Math.PI) cameraAngle -= 2 * Math.PI;
+                return { distance: distance * Math.cos(cameraAngle), color: hslColors[map[mapY][mapX]] };
             }
 
             px += dirX * stepSize;
             py += dirY * stepSize;
         }
-
-        return -1;
+        return { distance: Infinity, color: 0 };
     }
 
     const getRenderData = () => {
         const data = [];
 
         const before = new Date();
+        // fov -45 to 45
+        // 1deg = 0.017453 rad
         for (let i = player.a - (45 * 0.017453); i < player.a + (45 * 0.017453); i += 0.017453) {
-
-            data.push(castRay(i));
+            const d = castRay(i);
+            data.push(d)
         }
         const after = new Date();
         console.log("time took to calculate:", after.getTime() - before.getTime())
         return data;
     }
+    const keyDownHandler = (e) => {
+        //todo: pls optimize this shit
+        const newPlayer = { ...player }
+        if (e.key == 'a') {
+            newPlayer.a -= 0.1;
+            if (newPlayer.a < 0) {
+                newPlayer.a += 2 * Math.PI;
+            }
+        } else if (e.key == 'd') {
+            newPlayer.a += 0.1;
+            if (newPlayer.a > 2 * Math.PI) {
+                console.log(newPlayer.a)
+                newPlayer.a -= 2 * Math.PI;
+            }
+        } else if (e.key == 's') {
+
+            const dx = 10 * Math.cos(player.a);
+            const dy = 10 * Math.sin(player.a);
+            newPlayer.x -= dx;
+            newPlayer.y -= dy;
+        } else if (e.key == 'w') {
+            const dx = 10 * Math.cos(player.a);
+            const dy = 10 * Math.sin(player.a);
+
+            newPlayer.x += dx;
+            newPlayer.y += dy;
+        }
+        setPlayer(newPlayer)
+    }
+    // const renderGround = () =>{
+    //     const mx = player.x/scale;
+    //     const my = player.y/scale;
+    //     const data = getRenderData();
+    //     const heights = [];
+    //     return data.map(d=>(
+    //      <div style={{
+    //         height: `${Math.min(100, 100 * d.distance)}vh`,
+    //         width: '1.11vw',
+    //         backgroundColor:`${colors[floorMap[my][mx]]}`
+    //     }}></div>
+    //     ))
+    // }
+
 
     return (
         <div className="screen" tabIndex="0"
-            // onMouseMove={(e) => {
-
-            //     const newPlayer = { ...player }
-            //     newPlayer.a += e.movementX / 10;
-            //     setPlayer(newPlayer)
-            // }}
-            onKeyDown={(e) => {
-                //todo: pls optimize this shit
-                const newPlayer = { ...player }
-                if (e.key == 'a') {
-                    newPlayer.a -= 0.1;
-                    if (newPlayer.a < 0) {
-
-                        console.log(newPlayer.a)
-                        newPlayer.a += 2 * Math.PI;
-                    }
-                } else if (e.key == 'd') {
-
-                    newPlayer.a += 0.1;
-                    if (newPlayer.a > 2 * Math.PI) {
-                        console.log(newPlayer.a)
-                        newPlayer.a -= 2 * Math.PI;
-                    }
-                } else if (e.key == 's') {
-
-                    const dx = 10 * Math.cos(player.a);
-                    const dy = 10 * Math.sin(player.a);
-                    newPlayer.x -= dx;
-                    newPlayer.y -= dy;
-                } else if (e.key == 'w') {
-                    const dx = 10 * Math.cos(player.a);
-                    const dy = 10 * Math.sin(player.a);
-
-                    newPlayer.x += dx;
-                    newPlayer.y += dy;
-                }
-                setPlayer(newPlayer)
-            }}>
-            <Canvas data={getRenderData()}>
-
-            </Canvas>
-            <div className='ground' ></div>
+            onKeyDown={keyDownHandler}>
+            <Canvas data={getRenderData()} />
+            <div className='ground' >
+                {/* {renderGround()} */}
+            </div>
         </div>
     );
 }
